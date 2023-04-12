0d5877edfbad7178970c9ebf545356a8ce4159db
anazhmetdin <anazhmetdin@gmail.com>
2023-04-12 14:33:50 +0200

    fix empty claims

--- DOTNET/API/day3/IdentityPractice/IdentityPractice/Controllers/DataController.cs
+++ DOTNET/API/day3/IdentityPractice/IdentityPractice/Controllers/DataController.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityPractice.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,16 +17,30 @@ namespace IdentityPractice.Controllers
         }
 
         [HttpGet]
-        //[Authorize]
+        [Authorize]
+        public async Task<IActionResult> GetAuthData()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) { return Unauthorized(); }
+            return Ok(new { user.UserName, user.Email, user.HiringDate });
+        }
+
+        [HttpGet("Admin")]
+        [Authorize(policy: "AllowAdmin")]
+        public async Task<IActionResult> GetAdminData()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null) { return Unauthorized(); }
+            return Ok("You're admin");
+        }
+
+        [HttpGet("User")]
+        [Authorize(policy: "AllowUser")]
         public async Task<IActionResult> GetUserData()
         {
             var user = await _userManager.GetUserAsync(User);
-            if (user == null)
-            {
-                return Unauthorized();
-            }
-
-            return Ok(new { user.UserName, user.Email, user.HiringDate });
+            if (user == null) { return Unauthorized(); }
+            return Ok("You're user or admin");
         }
     }
 }
