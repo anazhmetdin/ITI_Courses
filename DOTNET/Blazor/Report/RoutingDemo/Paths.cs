d8667724f3656b32090e6ea6fac59c5c5b7a5593
anazhmetdin <anazhmetdin@gmail.com>
2023-05-12 19:24:23 +0300

    add blazor routing demo

---
+++ DOTNET/Blazor/Report/RoutingDemo/Paths.cs
@@ -0,0 +1,10 @@
+﻿namespace RoutingDemo
+{
+    public static class Paths
+    {
+        public const string counter = "/counter";
+        public const string id = "/{id:int}";
+        public const string component1 = "/component1";
+        public const string text = "/{text?}";
+    }
+}
